1922e4ae18792d22d02a4479546e85b4707cb904
Various <apple123@mail.ustc.edu.cn>
2015-03-27 18:35:51 +0800

    init

---
+++ LMProgressHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMProgressHUD
+//
+//  Created by Tim Geng on 3/27/15.
+//  Copyright (c) 2015 GF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
